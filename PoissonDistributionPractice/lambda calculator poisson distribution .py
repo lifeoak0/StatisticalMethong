a4d9472534a2a46edaaf4a417b801c289daf6e29
JLiu <107297778+lifeoak0@users.noreply.github.com>
2024-02-03 17:23:40 +0800

    Update lambda calculator poisson distribution .py

--- PoissonDistributionPractice/lambda calculator poisson distribution .py
+++ PoissonDistributionPractice/lambda calculator poisson distribution .py
@@ -1,8 +1,8 @@
-import numpy as np
+import numpy as np #调用numpy进行数值计算
 
 #随机数据示例
 data = np.array([2, 3, 4, 3, 1, 2, 0, 2, 3, 2, 1, 1, 1, 0, 4, 3, 2])
 
-# 计算平均值作为λ的估计（lambda在泊松分布中代表的是时间在固定时间间隔和空间区域发生的平均次数）
+# 计算平均值作为λ的估计（lambda在泊松分布中代表的是时间在固定时间间隔和空间区域发生的平均次数，其实就是期望值）
 lambda_estimate = np.mean(data)
 print("Estimated λ for the Poisson distribution:", lambda_estimate)
